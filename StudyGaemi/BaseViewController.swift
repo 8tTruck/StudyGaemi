f8598976c11e125a492a687a9ed5fb0b946c067d
신승현 <sinseunghyeon313@gmail.com>
2024-05-29 20:51:52 +0900

    [Setting] 초기 설정

---
+++ StudyGaemi/BaseViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BaseViewController.swift
+//  StudyGaemi
+//
+//  Created by t2023-m0056 on 5/29/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func configureUI() {
+        // UIButton이나 UILabel 등과 같은 부분 초기 설정 함수
+    }
+    
+    private func constraintLayout() {
+        // UIButton이나 UILabel 등과 같은 부분 제약조건 설정 함수
+    }
+
+}
