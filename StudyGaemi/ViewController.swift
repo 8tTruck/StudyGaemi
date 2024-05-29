21660878b3643fffcca065714511cf510a3e5bbf
신승현 <sinseunghyeon313@gmail.com>
2024-05-29 20:48:25 +0900

    Initial Commit

---
+++ StudyGaemi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyGaemi
+//
+//  Created by t2023-m0056 on 5/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
