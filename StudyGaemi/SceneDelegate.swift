2c00f5ae321802b2fcf9b1e5681d71faf774549b
신승현 <sinseunghyeon313@gmail.com>
2024-06-03 20:28:17 +0900

    [Fix] 처음 실행화면 탭바뷰로 수정

--- StudyGaemi/SceneDelegate.swift
+++ StudyGaemi/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let bottomTabBarController = LoginViewController()
+        let bottomTabBarController = BottomTabBarViewController()
         
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = bottomTabBarController
