8bb8d0ce049490059325befec5135dc6c3b93d34
jiyeondu <shinjy8776@gmail.com>
2024-06-11 18:39:05 +0900

    Feat: CalendarView algo

--- StudyGaemi/Views/CalendarViewController.swift
+++ StudyGaemi/Views/CalendarViewController.swift
@@ -33,7 +33,6 @@ enum status{
     case perfect
     case study
     case wakeup
-    case none
 }
 
 class CalendarViewController: BaseViewController {
@@ -53,9 +52,30 @@ class CalendarViewController: BaseViewController {
     private var perfectAntCount = 0
     private var studyAntCount = 0
     private var wakeupAntCount = 0
-    private var result: [badgeStatus] = [.perfect, .study, .wakeup]
+    private var result: [status] = []
     private var currentPageYearAndMonth: String = ""
-    private var monthlyResult: [status] = []
+    private var monthlyResultDict: [[Date: status]] = []
+    private var tableHeight = 300 {
+        didSet {
+            badgeView.snp.removeConstraints()
+            badgeView.snp.makeConstraints { make in
+                make.top.equalTo(calendarbackView.snp.bottom).offset(15)
+                make.leading.trailing.equalToSuperview().inset(20)
+                make.height.equalTo(tableHeight)
+                make.bottom.equalToSuperview().inset(10)
+            }
+            badgeView.reloadData()
+        }
+    }
+    private var perfectAntTotalCount = 0 {
+        didSet {
+            let currentPage = calendarView.currentPage
+            let calendar = Calendar.current
+            guard let range = calendar.range(of: .day, in: .month, for: currentPage) else { return }
+            let numberOfDays = range.count
+            updateCustomHeaderView(currentPage, numberOfDays)
+        }
+    }
     
     private let titleLabel = UILabel().then {
         $0.text = "월간개미"
@@ -214,21 +234,34 @@ class CalendarViewController: BaseViewController {
         calCurrentYearAndMonth()
         
         studies = [
+            study(email: "user6@example.com", date: dateFormatter.date(from: "2024/05/10")!, success: true),
+            study(email: "user7@example.com", date: dateFormatter.date(from: "2024/05/05")!, success: true),
             study(email: "user6@example.com", date: dateFormatter.date(from: "2024/06/01")!, success: true),
             study(email: "user7@example.com", date: dateFormatter.date(from: "2024/06/02")!, success: true),
             study(email: "user8@example.com", date: dateFormatter.date(from: "2024/06/03")!, success: true),
             study(email: "user9@example.com", date: dateFormatter.date(from: "2024/06/04")!, success: false),
-            study(email: "user10@example.com", date: dateFormatter.date(from: "2024/06/05")!, success: false)
+            study(email: "user10@example.com", date: dateFormatter.date(from: "2024/06/05")!, success: false),
+            study(email: "user6@example.com", date: dateFormatter.date(from: "2024/06/06")!, success: true),
+            study(email: "user7@example.com", date: dateFormatter.date(from: "2024/06/07")!, success: true),
+            study(email: "user8@example.com", date: dateFormatter.date(from: "2024/06/08")!, success: true),
+            study(email: "user9@example.com", date: dateFormatter.date(from: "2024/06/09")!, success: false),
+            study(email: "user9@example.com", date: dateFormatter.date(from: "2024/06/10")!, success: false)
         ]
         wakeups = [
             wakeup(email: "user6@example.com", date: dateFormatter.date(from: "2024/06/01")!, success: true),
             wakeup(email: "user7@example.com", date: dateFormatter.date(from: "2024/06/02")!, success: true),
             wakeup(email: "user8@example.com", date: dateFormatter.date(from: "2024/06/03")!, success: true),
             wakeup(email: "user9@example.com", date: dateFormatter.date(from: "2024/06/04")!, success: true),
-            wakeup(email: "user10@example.com", date: dateFormatter.date(from: "2024/06/05")!, success: true)
+            wakeup(email: "user10@example.com", date: dateFormatter.date(from: "2024/06/05")!, success: true),
+            wakeup(email: "user6@example.com", date: dateFormatter.date(from: "2024/06/06")!, success: true),
+            wakeup(email: "user7@example.com", date: dateFormatter.date(from: "2024/06/07")!, success: true),
+            wakeup(email: "user8@example.com", date: dateFormatter.date(from: "2024/06/08")!, success: false),
+            wakeup(email: "user9@example.com", date: dateFormatter.date(from: "2024/06/09")!, success: true),
+            wakeup(email: "user10@example.com", date: dateFormatter.date(from: "2024/06/10")!, success: true)
+            
         ]
         
-        calculateThisMonthAnt(yearAndMonth: currentPageYearAndMonth)
+        updateData()
     }
     
     // MARK: - method
@@ -293,8 +326,8 @@ class CalendarViewController: BaseViewController {
         badgeView.snp.makeConstraints { make in
             make.top.equalTo(calendarbackView.snp.bottom).offset(15)
             make.leading.trailing.equalToSuperview().inset(20)
-            make.height.equalTo(282)
-            make.bottom.equalToSuperview().inset(30)
+            make.height.equalTo(tableHeight)
+            make.bottom.equalToSuperview().inset(10)
         }
         
         customHeaderView.snp.makeConstraints { make in
@@ -315,68 +348,151 @@ class CalendarViewController: BaseViewController {
         }
     }
     
+    
     private func updateCustomHeaderView(_ month: Date, _ days: Int){
-        //한달에 완벽개미 계산하는 로직
         monthLabel.text = headerDateFormatter.string(from: month)
-        totalLabel.text = "/\(days)"
+        totalLabel.text = "\(perfectAntTotalCount)/\(days)"
     }
     
-    //특정 일의 완벽개미, 공부개미, 기상개미 측정
-    private func calculateThisMonthAnt(yearAndMonth: String){
+    private func updateData(){
+        result = []
+        monthlyResultDict = []
+        monthlyResultDict = calculateThisMonthAnt(yearAndMonth: currentPageYearAndMonth)
+        calculateStraightForPerfectBadge()
+        calculateStraightForStudyBadge()
+        calculateStraightForWakeupBadge()
+        print("studyAntCount \(studyAntCount) perfectAntCount \(perfectAntCount)")
+    }
+    
+    //이달의 완벽개미, 공부개미, 기상개미 측정
+    private func calculateThisMonthAnt(yearAndMonth: String) -> [[Date: status]] {
+        
+        
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ko_KR")
         dateFormatter.dateFormat = "yy.MM"
         
         guard let yearMonthDate = dateFormatter.date(from: yearAndMonth) else {
             print("Invalid year and month format")
-            return
+            return []
         }
         
-        let calendar = Calendar.current
-        let range = calendar.range(of: .day, in: .month, for: yearMonthDate)!
-        let components = calendar.dateComponents([.year, .month], from: yearMonthDate)
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        var monthlyResult: [status] = []
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(identifier: "Asia/Seoul")!
         
+        let range = calendar.range(of: .day, in: .month, for: yearMonthDate)!
+        var components = calendar.dateComponents([.year, .month], from: yearMonthDate)
+        
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+                
         for day in range {
-            var dayComponents = components
-            dayComponents.day = day
-            guard let date = calendar.date(from: dayComponents) else{ return }
+            var resultDict: [Date: status] = [:]
+            components.day = day
+            guard let date = calendar.date(from: components) else { continue }
             
             let matchingStudies = studies.filter { $0.date == date }
-            
-            if matchingStudies.isEmpty {
-                monthlyResult.append(.none)
-            } else {
+            if !matchingStudies.isEmpty{
                 let successfulStudy = matchingStudies.contains { $0.success }
-                
                 if successfulStudy {
-                    
-                    monthlyResult.append(.study)
-                } else {
-                    monthlyResult.append(.none)
+                    resultDict[date] = .study
                 }
             }
             
             let matchingWakeups = wakeups.filter { $0.date == date }
             if !matchingWakeups.isEmpty {
                 let successfulWakeup = matchingWakeups.contains { $0.success }
-                if successfulWakeup{
-                    
-                    if monthlyResult.popLast() == .study {
-                        
-                        monthlyResult.append(.perfect)
-                        
+                if successfulWakeup {
+                    if let previousResult = resultDict[date], previousResult == .study {
+                        resultDict[date] = .perfect
                     } else {
-                        
-                        monthlyResult.append(.wakeup)
-                        
+                        resultDict[date] = .wakeup
                     }
                 }
             }
-            
+            if !resultDict.isEmpty {
+                monthlyResultDict.append(resultDict)
+            }
         }
-        print(monthlyResult)
+        
+        monthlyResultDict.sort { (dict1, dict2) in
+            guard let date1 = dict1.keys.first, let date2 = dict2.keys.first else {
+                return false // 빈 거 고려
+            }
+            return date1 < date2
+        }
+        return monthlyResultDict
+    }
+    
+    //연속일 계산
+    private func calculateStraightForPerfectBadge(){
+        var max = 0
+        var temp = 0
+        var count = 0
+        var yesterday = Date()
+        for dic in monthlyResultDict {
+            for (day, value) in dic {
+                if value == .perfect {
+                    temp += 1
+                    max = temp > max ? temp : max
+                    perfectAntTotalCount += 1
+                    yesterday = day
+                } else {
+                    //max = temp > max ? temp : max
+                    temp = 0
+                }
+            }
+        }
+        perfectAntTotalCount = count
+        perfectAntCount = 0
+        if max != 0{
+            result.append(.perfect)
+            perfectAntCount = max
+        }
+        
+    }
+    
+    private func calculateStraightForStudyBadge(){
+        var max = 0
+        var temp = 0
+        for dic in monthlyResultDict {
+            for (_, value) in dic {
+                if value == .study {
+                    temp += 1
+                    max = temp > max ? temp : max
+                } else {
+                    //max = temp > max ? temp : max
+                    temp = 0
+                }
+            }
+        }
+        studyAntCount = 0
+        if max != 0{
+            result.append(.study)
+            studyAntCount = max
+        }
+        
+    }
+    
+    private func calculateStraightForWakeupBadge(){
+        var max = 0
+        var temp = 0
+        for dic in monthlyResultDict {
+            for (_, value) in dic {
+                if value == .wakeup {
+                    temp += 1
+                    max = temp > max ? temp : max
+                } else {
+                    //max = temp > max ? temp : max
+                    temp = 0
+                }
+            }
+        }
+        wakeupAntCount = 0
+        if max != 0{
+            result.append(.wakeup)
+            wakeupAntCount = max
+        }
     }
     
     //오늘 달과 날짜 계산
@@ -394,6 +510,7 @@ class CalendarViewController: BaseViewController {
 // MARK: - extension
 extension CalendarViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
         return result.count
     }
     
@@ -435,10 +552,23 @@ extension CalendarViewController: FSCalendarDelegate, FSCalendarDataSource, FSCa
     
     func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell{
         guard let cell = calendar.dequeueReusableCell(withIdentifier: "CalendarCell", for: date, at: position) as? CalendarCell else { return FSCalendarCell() }
-        //        switch monthlyResult[indexPath.row]{
-        //        case .perfect:
-        //            cell[indexPath.row].image =
-        //        }
+        let calendar = Calendar.current
+        
+        for resultDict in monthlyResultDict {
+            for (dat, status) in resultDict {
+                if calendar.isDate(dat, inSameDayAs: date) {
+                    switch status {
+                    case .perfect:
+                        cell.badgeView.image = UIImage.perfectStamp
+                    case .study:
+                        cell.badgeView.image = UIImage.studyStamp
+                    case .wakeup:
+                        cell.badgeView.image = UIImage.wakeupStamp
+                    }
+                }
+            }
+        }
+        
         return cell
     }
     
@@ -474,6 +604,13 @@ extension CalendarViewController: FSCalendarDelegate, FSCalendarDataSource, FSCa
         formatter.dateFormat = "yy.MM"
         currentPageYearAndMonth = formatter.string(from: currentPage)
         
+        updateData()
+        
+        calendarView.reloadData()
+        
+        print("============== \n \(result)")
+        
+        tableHeight = Int(CGFloat(result.count) * 100.0)
         badgeView.reloadData()
     }
 }
