d183501f725d10ff8078b20ba37609dbabfcd016
sngsp <ssleerok@gmail.com>
2024-06-14 14:34:23 +0900

    [Fix] Conflict 해결

--- StudyGaemi/Views/MakePasswordViewController.swift
+++ StudyGaemi/Views/MakePasswordViewController.swift
@@ -564,6 +564,8 @@ class MakePasswordViewController: UIViewController {
             return
         }
         
+        FirestoreManager.shared.createUserData(email: email, nickName: email, loginMethod: "Firebase")
+        
         AuthenticationManager.shared.createUser(email: email, password: password)
         let nextVC = EmailConfirmViewController()
         nextVC.modalPresentationStyle = .fullScreen
@@ -574,6 +576,5 @@ class MakePasswordViewController: UIViewController {
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "확인", style: .default))
         present(alert, animated: true)
-        FirestoreManager.shared.createUserData(email: email, nickName: email, loginMethod: "Firebase")
     }
 }
